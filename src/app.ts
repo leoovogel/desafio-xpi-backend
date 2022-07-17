e872e782237953af41015f24f9a7c258b5535bdf
Leonardo Vogel <leeo.vog13@gmail.com>
2022-07-17 09:12:45 -0300

    feat: creates middleware to handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
+
+import errorMiddleware from './middlewares/errorHandler';
 import { attachRouters } from './routes';
 
 const app = express();
@@ -6,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 attachRouters(app);
+
+app.use(errorMiddleware);
 
 export default app;
