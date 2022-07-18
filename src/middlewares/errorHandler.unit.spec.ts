d37196ae452dcc787065e6bfaf3bd590def6eeef
Leonardo Vogel <leeo.vog13@gmail.com>
2022-07-18 03:21:10 -0300

    test(unit-test): create unit test for error middleware

---
+++ src/middlewares/errorHandler.unit.spec.ts
@@ -0,0 +1,23 @@
+import { StatusCodes } from 'http-status-codes';
+import * as builders from '../../tests/builders';
+import errorMiddleware from './errorHandler';
+
+describe('Error handler middleware', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should return status code 400 and error message when an error is triggered', () => {
+    const err = builders.buildError(StatusCodes.BAD_REQUEST, 'Error message');
+    const req = builders.buildReq();
+    const res = builders.buildRes();
+    const next = builders.buildNext();
+
+    errorMiddleware(err, req, res, next);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Error message' });
+  });
+});
